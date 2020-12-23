a41488fb0c228b046fd84f7b256b7896a7d3cb57
hageyobigun <skei081518@gmail.com>
2020-12-24 02:39:20 +0900

    選択・サイズ変更可能

--- Assets/Scripts/Player/PlayerBlock.cs
+++ Assets/Scripts/Player/PlayerBlock.cs
@@ -1,16 +1,23 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerBlock : MonoBehaviour
 {
     [SerializeField]private GameObject block = null;
     [SerializeField]private Transform  blockParent = null;
+    private SpriteRenderer spriteRenderer;
+
+    public void Start()
+    {
+        spriteRenderer = block.GetComponent<SpriteRenderer>();
+    }
+
 
     //穴をあける(ブロックを置く)
-    public void OpenHole()
+    public void OpenHole(Sprite playerSprite)
     {
+        spriteRenderer.sprite = playerSprite;//プレイヤーの形に合わせる
         var blockClone = Instantiate(block, transform.position, Quaternion.identity).transform;
         blockClone.SetParent(blockParent, true);
     }
+
 }
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UniRx;
 using UniRx.Triggers;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     private PlayerInput playerInput;
     private PlayerMove playerMove;
     private PlayerBlock playerBlock;
+    private SpriteRenderer playerSprite;
+    [SerializeField] private Image selectBlock = null;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +19,7 @@ public class PlayerController : MonoBehaviour
         playerInput = new PlayerInput();
         playerMove = new PlayerMove(this.gameObject);
         playerBlock = GetComponent<PlayerBlock>();
+        playerSprite = GetComponent<SpriteRenderer>();
 
         //移動
         this.UpdateAsObservable()
@@ -24,7 +28,11 @@ public class PlayerController : MonoBehaviour
         //穴をあける(ブロックを置く)
         this.UpdateAsObservable()
             .Where(_ => playerInput.IsMouseButton())
-            .Subscribe(_ => playerBlock.OpenHole());
-        
+            .Subscribe(_ => playerBlock.OpenHole(playerSprite.sprite));
+
+        //sprite切り替え
+        this.UpdateAsObservable()
+            .Subscribe(_ => playerSprite.sprite = selectBlock.sprite);
+
     }
 }
